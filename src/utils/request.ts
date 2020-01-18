a198be5e9fda1f0c2b7847c81373ccfb6a427368
“yanxlg <xlyan@vova.com.hk>
2020-01-18 18:06:12 +0800

    update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,8 @@
-import { message, notification } from 'antd';
 /**
  * request 网络请求工具
  * 更详细的 api 文档: https://github.com/umijs/umi-request
  */
+import { message, notification } from 'antd';
 import { extend, RequestOptionsInit, ResponseError } from 'umi-request';
 import { router } from 'umi';
 import { parse, stringify } from 'querystring';
