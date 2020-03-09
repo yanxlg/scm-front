d921d6a8603308cb47bb56c9852391e7e39a4532
xlyan <xlyan@vova.com.hk>
2020-03-09 17:06:06 +0800

    format

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import BasicLayout from './BasicLayout';
 
-export default function(props:any) {
+export default function(props: any) {
     const pathname = props.location.pathname;
-    if (pathname === '/login' || pathname === "/404" || pathname === "/500") {
+    if (pathname === '/login' || pathname === '/404' || pathname === '/500') {
         return props.children;
     }
-    return (
-        <BasicLayout {...props}/>
-    )
+    return <BasicLayout {...props} />;
 }
