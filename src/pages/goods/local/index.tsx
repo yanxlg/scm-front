e2d9b947e82e2c8ea95e88dc21cdf2985410c64d
bbhuang <bbhuang@vova.com.hk>
2020-03-20 18:44:34 +0800

    'fix-超级一键上架'

--- src/pages/goods/local/index.tsx
+++ src/pages/goods/local/index.tsx
@@ -18,7 +18,7 @@ import {
     getGoodsDelete,
     IFilterParams,
     getCatagoryList,
-    getAllGoodsOnsale
+    postAllGoodsOnsale
 } from '@/services/goods';
 import { strToNumber, getCurrentPage } from '@/utils/common';
 import { RouteComponentProps } from 'dva/router';
@@ -499,15 +499,15 @@ class Local extends React.PureComponent<LocalPageProps, IIndexState> {
     };
 
     // 查询商品一键上架
-    getAllGoodsOnsale = () => {
+    postAllGoodsOnsale = () => {
         const searchParams = this.getSearchParams();
         if (searchParams) {
             this.setState({
                 allOnsaleLoading: true
             })
-            getAllGoodsOnsale(searchParams)
+            postAllGoodsOnsale(searchParams)
                 .then(res => {
-                    // console.log('getAllGoodsOnsale', res);
+                    // console.log('postAllGoodsOnsale', res);
                     this.onSearch();
                     message.success('查询商品一键上架成功');
                 })
@@ -640,7 +640,7 @@ class Local extends React.PureComponent<LocalPageProps, IIndexState> {
                     getGoodsDelete={this.getGoodsDelete}
                     toggleExcelDialog={this.toggleExcelDialog}
                     getCurrentCatagory={this.getCurrentCatagory}
-                    getAllGoodsOnsale={this.getAllGoodsOnsale}
+                    postAllGoodsOnsale={this.postAllGoodsOnsale}
                 />
                 <Pagination
                     className="goods-local-pagination"
