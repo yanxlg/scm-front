a1f11869f45748b1680ed98436e9e381d6b59f52
bbhuang <bbhuang@vova.com.hk>
2020-04-09 11:37:22 +0800

    '4期开发'

--- src/pages/goods/local/components/HistoryPane.tsx
+++ src/pages/goods/local/components/HistoryPane.tsx
@@ -18,152 +18,150 @@ const HistoryPane: React.FC<IProps> = ({ commodityId }) => {
     const [loading, setLoading] = useState(false);
     const [parentDialogStatus, setParentDialogStatus] = useState(false);
     const [goodsList, setGoodsList] = useState<IGoodsAndSkuItem[]>([]);
-    const {
-        page,
-        setPage,
-        pageSize,
-        setPageSize,
-        total,
-        setTotal
-    } = usePagination();
+    const [productId, setProductId] = useState('');
 
-    const hideParentDialog = useCallback(
-        () => {
-            setParentDialogStatus(false);
-        },
-        []
-    );
-    
-    const getCurrentList = useCallback(
-        () => {
-            setLoading(true);
-            getGoodsVersion({
-                page: 1,
-                page_count: 50,
-                commodity_id: commodityId
-            }).then(res => {
+    const { page, setPage, pageSize, setPageSize, total, setTotal } = usePagination();
+
+    const showParentDialog = useCallback(productId => {
+        setParentDialogStatus(true);
+        setProductId(commodityId);
+    }, []);
+
+    const hideParentDialog = useCallback(() => {
+        setParentDialogStatus(false);
+    }, []);
+
+    const getCurrentList = useCallback(() => {
+        setLoading(true);
+        getGoodsVersion({
+            page: 1,
+            page_count: 50,
+            commodity_id: commodityId,
+        })
+            .then(res => {
                 // console.log('getCurrentList', res);
                 const { list } = res.data;
-                setGoodsList(list.map(item => {
-                    const { sku_info, update_time, ...rest  } = item;
-                    const _update_time = utcToLocal(update_time);
-                    if (sku_info?.length > 0) {
+                setGoodsList(
+                    list.map(item => {
+                        const { sku_info, update_time, ...rest } = item;
+                        const _update_time = utcToLocal(update_time);
+                        if (sku_info?.length > 0) {
+                            return {
+                                _update_time,
+                                ...item,
+                                ...sku_info[0],
+                            };
+                        }
                         return {
                             _update_time,
                             ...item,
-                            ...sku_info[0]
-                        }
-                    }
-                    return {
-                        _update_time,
-                        ...item
-                    };
-                }))
-            }).finally(() => {
+                        };
+                    }),
+                );
+            })
+            .finally(() => {
                 setLoading(false);
             });
-        },
-        [],
-    );
+    }, []);
 
-    const columns = useMemo<ColumnType<IGoodsAndSkuItem>[]>(
-        () => {
-            return [
-                {
-                    title: 'Product ID',
-                    dataIndex: 'product_id',
-                    width: 140,
-                    align: 'center',
-                    render: (val: string) => {
-                        return (
-                            <>
-                                {val}
-                                <Button type="link" onClick={() => setParentDialogStatus(true)}>查看父版本</Button>
-                            </>
-                        )
-                    }
+    const columns = useMemo<ColumnType<IGoodsAndSkuItem>[]>(() => {
+        return [
+            {
+                title: 'Product ID',
+                dataIndex: 'product_id',
+                width: 140,
+                align: 'center',
+                render: (val: string) => {
+                    return (
+                        <>
+                            {val}
+                            <Button type="link" onClick={() => showParentDialog(val)}>
+                                查看父版本
+                            </Button>
+                        </>
+                    );
                 },
-                {
-                    title: '版本状态',
-                    dataIndex: 'goods_status',
-                    width: 120,
-                    align: 'center',
+            },
+            {
+                title: '版本状态',
+                dataIndex: 'goods_status',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '上架渠道',
+                dataIndex: 'onsale_info',
+                align: 'center',
+                width: 120,
+                render: (value: IOnsaleItem[]) => {
+                    return [...new Set(value.map(item => item.onsale_channel))].map(channel => (
+                        <div key={channel}>{channel}</div>
+                    ));
                 },
-                {
-                    title: '上架渠道',
-                    dataIndex: 'onsale_info',
-                    align: 'center',
-                    width: 120,
-                    render: (value: IOnsaleItem[]) => {
-                        return [...new Set(value.map(item => item.onsale_channel))].map(channel => (
-                            <div key={channel}>{channel}</div>
-                        ));
-                    },
+            },
+            {
+                title: '销售状态',
+                dataIndex: 'inventory_status',
+                align: 'center',
+                width: 120,
+                render: (val: string) => (val == '1' ? '可销售' : '不可销售'),
+            },
+            {
+                title: '商品图片',
+                dataIndex: 'goods_img',
+                align: 'center',
+                width: 120,
+                render: (val: string) => <AutoEnLargeImg src={val} className={styles.goodsImg} />,
+            },
+            {
+                title: '商品名称',
+                dataIndex: 'title',
+                width: 160,
+                align: 'center',
+            },
+            {
+                title: '商品描述',
+                dataIndex: 'description',
+                width: 200,
+                align: 'center',
+            },
+            {
+                title: '商品分类',
+                dataIndex: 'first_catagory',
+                align: 'center',
+                width: 160,
+                render: (_, row: IGoodsAndSkuItem) => {
+                    const { first_catagory, second_catagory, third_catagory } = row;
+                    return `${first_catagory.name || ''}-${second_catagory.name ||
+                        ''}-${third_catagory.name || ''}`;
                 },
-                {
-                    title: '销售状态',
-                    dataIndex: 'inventory_status',
-                    align: 'center',
-                    width: 120,
-                    render: (val: string) => val == '1' ? '可销售' : '不可销售'
-                },
-                {
-                    title: '商品图片',
-                    dataIndex: 'goods_img',
-                    align: 'center',
-                    width: 120,
-                    render: (val: string) => <AutoEnLargeImg src={val} className={styles.goodsImg} />
-                },
-                {
-                    title: '商品名称',
-                    dataIndex: 'title',
-                    width: 160,
-                    align: 'center',
-                },
-                {
-                    title: '商品描述',
-                    dataIndex: 'description',
-                    width: 200,
-                    align: 'center',
-                },
-                {
-                    title: '商品分类',
-                    dataIndex: 'first_catagory',
-                    align: 'center',
-                    width: 160,
-                    render: (_, row: IGoodsAndSkuItem) => {
-                        const { first_catagory, second_catagory, third_catagory } = row;
-                        return `${first_catagory.name || ''}-${second_catagory.name || ''}-${third_catagory.name || ''}`;
-                    }
-                },
-                {
-                    title: 'sku数量',
-                    dataIndex: 'sku_number',
-                    width: 120,
-                    align: 'center',
-                },
-                {
-                    title: '爬虫价格(￥)',
-                    dataIndex: '',
-                    width: 120,
-                    align: 'center',
-                },
-                {
-                    title: '销量',
-                    dataIndex: 'sales_volume',
-                    width: 120,
-                    align: 'center',
-                },
-                {
-                    title: '变更时间',
-                    dataIndex: '_update_time',
-                    width: 120,
-                    align: 'center',
-                },
-            ];
-        },
-        []
-    );
+            },
+            {
+                title: 'sku数量',
+                dataIndex: 'sku_number',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '爬虫价格(￥)',
+                dataIndex: '',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '销量',
+                dataIndex: 'sales_volume',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '变更时间',
+                dataIndex: '_update_time',
+                width: 120,
+                align: 'center',
+            },
+        ];
+    }, []);
 
     useEffect(() => {
         getCurrentList();
@@ -192,13 +190,14 @@ const HistoryPane: React.FC<IProps> = ({ commodityId }) => {
                         showTotal={total => `共 ${total} 条`}
                     />
                 </div>
-                <VersionParentDialog 
+                <VersionParentDialog
                     visible={parentDialogStatus}
                     hideModal={hideParentDialog}
+                    productId={productId}
                 />
             </>
-        )
+        );
     }, [loading, goodsList, parentDialogStatus]);
-}
+};
 
 export default HistoryPane;
--- src/pages/goods/local/components/VersionParentDialog.tsx
+++ src/pages/goods/local/components/VersionParentDialog.tsx
@@ -1,22 +1,160 @@
-import React, { useMemo, useState, useCallback } from 'react';
+import React, { useMemo, useState, useCallback, useEffect } from 'react';
 import { Modal } from 'antd';
-import { FitTable } from 'react-components';
+import { getGoodsVersion } from '@/services/goods';
+import { IGoodsAndSkuItem, IOnsaleItem } from '@/interface/ILocalGoods';
+import { utcToLocal } from '@/utils/date';
+import { ColumnType } from 'antd/lib/table';
+import { AutoEnLargeImg, FitTable } from 'react-components';
+
+import styles from '../_version.less';
 
 interface IProps {
     visible: boolean;
+    productId: string;
     hideModal(): void;
 }
 
-const VersionParentDialog: React.FC<IProps> = ({
-    visible,
-    hideModal
-}) => {
+const VersionParentDialog: React.FC<IProps> = ({ visible, hideModal, productId }) => {
+    const [loading, setLoading] = useState(false);
+    const [goodsList, setGoodsList] = useState<IGoodsAndSkuItem[]>();
 
-    const [goodsList, setGoodsList] = useState();
+    const getCurrentList = useCallback(() => {
+        setLoading(true);
+        getGoodsVersion({
+            page: 1,
+            page_count: 50,
+            commodity_id: productId,
+        })
+            .then(res => {
+                // console.log('getCurrentList', res);
+                const { list } = res.data;
+                setGoodsList(
+                    list.map(item => {
+                        const { sku_info, update_time, ...rest } = item;
+                        const _update_time = utcToLocal(update_time);
+                        if (sku_info?.length > 0) {
+                            return {
+                                _update_time,
+                                ...item,
+                                ...sku_info[0],
+                            };
+                        }
+                        return {
+                            _update_time,
+                            ...item,
+                        };
+                    }),
+                );
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [productId]);
 
     const onCancel = useCallback(() => {
         hideModal();
     }, []);
+
+    const columns = useMemo<ColumnType<IGoodsAndSkuItem>[]>(() => {
+        return [
+            {
+                title: '类型',
+                // dataIndex: 'product_id',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: 'Product ID',
+                dataIndex: 'product_id',
+                width: 140,
+                align: 'center',
+            },
+            {
+                title: '版本状态',
+                dataIndex: 'goods_status',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '上架渠道',
+                dataIndex: 'onsale_info',
+                align: 'center',
+                width: 120,
+                render: (value: IOnsaleItem[]) => {
+                    return [...new Set(value.map(item => item.onsale_channel))].map(channel => (
+                        <div key={channel}>{channel}</div>
+                    ));
+                },
+            },
+            {
+                title: '销售状态',
+                dataIndex: 'inventory_status',
+                align: 'center',
+                width: 120,
+                render: (val: string) => (val == '1' ? '可销售' : '不可销售'),
+            },
+            {
+                title: '商品图片',
+                dataIndex: 'goods_img',
+                align: 'center',
+                width: 120,
+                render: (val: string) => <AutoEnLargeImg src={val} className={styles.goodsImg} />,
+            },
+            {
+                title: '商品名称',
+                dataIndex: 'title',
+                width: 160,
+                align: 'center',
+            },
+            {
+                title: '商品描述',
+                dataIndex: 'description',
+                width: 200,
+                align: 'center',
+            },
+            {
+                title: '商品分类',
+                dataIndex: 'first_catagory',
+                align: 'center',
+                width: 160,
+                render: (_, row: IGoodsAndSkuItem) => {
+                    const { first_catagory, second_catagory, third_catagory } = row;
+                    return `${first_catagory.name || ''}-${second_catagory.name ||
+                        ''}-${third_catagory.name || ''}`;
+                },
+            },
+            {
+                title: 'sku数量',
+                dataIndex: 'sku_number',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '爬虫价格(￥)',
+                dataIndex: '',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '销量',
+                dataIndex: 'sales_volume',
+                width: 120,
+                align: 'center',
+            },
+            {
+                title: '变更时间',
+                dataIndex: '_update_time',
+                width: 120,
+                align: 'center',
+            },
+        ];
+    }, []);
+
+    useEffect(() => {
+        if (productId) {
+            getCurrentList();
+        }
+    }, [productId]);
 
     return useMemo(() => {
         return (
@@ -27,10 +165,19 @@ const VersionParentDialog: React.FC<IProps> = ({
                 footer={null}
                 onCancel={onCancel}
             >
-                Modal
+                <FitTable
+                    bordered
+                    rowKey="product_id"
+                    loading={loading}
+                    columns={columns}
+                    dataSource={goodsList}
+                    scroll={{ x: 'max-content' }}
+                    pagination={false}
+                    autoFitY={false}
+                />
             </Modal>
-        )
-    }, [visible]);
-}
+        );
+    }, [visible, loading, goodsList]);
+};
 
 export default VersionParentDialog;
