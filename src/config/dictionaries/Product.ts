72dbe224546e34e0f1cbc8898e5264bbe7708e3d
xlyan <xlyan@vova.com.hk>
2020-05-11 09:04:46 +0800

    add state

--- src/config/dictionaries/Product.ts
+++ src/config/dictionaries/Product.ts
@@ -11,6 +11,14 @@ export const ProductStatusMap = {
 export type ProductStatusCode = keyof typeof ProductStatusMap;
 
 export const ProductStatusList = transStatusList(ProductStatusMap);
+
+export const ProductStatusResponseMap = {
+    0: '上传失败',
+    1: '待上架',
+    2: '已上架',
+    3: '已下架',
+    5: '上传中',
+};
 // 返回哪些状态可以执行上架操作
 export const checkUpperShelf = function(status: number) {
     return status === 1 || status === 3; // 可上架
