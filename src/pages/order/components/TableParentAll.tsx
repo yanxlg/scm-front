fa97a97842c3bc18c5cd1bad7d1b858cf81258b0
xlyan <xlyan@vova.com.hk>
2020-05-14 17:55:32 +0800

    fix

--- src/pages/order/components/TableParentAll.tsx
+++ src/pages/order/components/TableParentAll.tsx
@@ -227,22 +227,6 @@ class TableParentAll extends React.PureComponent<IProps, IState> {
             render: this.mergeCell,
             defaultHide: true,
         },
-        {
-            key: 'saleMinusPurchaseNormalPrice',
-            title: '销售-采购价差',
-            dataIndex: 'saleMinusPurchaseNormalPrice',
-            align: 'center',
-            width: 180,
-            render: (value, row: IChildOrderItem) => {
-                const { productPrice = 0, purchaseNormalPrice = 0, purchaseNumber = 0 } = row;
-                const purchasePrice = Number(purchaseNormalPrice);
-                if (purchasePrice === 0 || isNaN(purchasePrice)) {
-                    return '';
-                }
-                const result = (Number(productPrice) - purchasePrice) * Number(purchaseNumber);
-                return result < 0 ? <span style={{ color: 'red' }}>{result}</span> : result;
-            },
-        },
     ];
 
     constructor(props: IProps) {
