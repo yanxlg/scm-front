d825d776fc41aa35d7046b30b729b4e6ff3e5fac
xlyan <xlyan@vova.com.hk>
2020-06-15 15:38:48 +0800

    format

--- src/services/order.ts
+++ src/services/order.ts
@@ -35,7 +35,10 @@ export async function filterOrder(params: IFilterProps) {
     });
 }
 
-export async function modifyOrderInfo(params: { salesOrderGoodsSn: string; purchaseTrackingNumber?: string; }) {
+export async function modifyOrderInfo(params: {
+    salesOrderGoodsSn: string;
+    purchaseTrackingNumber?: string;
+}) {
     return request.post(ApiPathEnum.ModifyOrderInfo, {
         requestType: 'form',
         data: params,
@@ -49,17 +52,14 @@ export async function cancelOrder(params: { salesOrderGoodsSn: string }) {
     });
 }
 
-export async function modifyMark(params: {
-    salesOrderGoodsSn: string,
-    remark: string
-}) {
+export async function modifyMark(params: { salesOrderGoodsSn: string; remark: string }) {
     return request.post(ApiPathEnum.ModifyMark, {
         requestType: 'form',
         data: params,
     });
 }
 
-export async function manualCreatePurchaseOrder(params:{salesOrderGoodsSns:string}) {
+export async function manualCreatePurchaseOrder(params: { salesOrderGoodsSns: string }) {
     return request.post(ApiPathEnum.ManualCreatePurchaseOrder, {
         requestType: 'form',
         data: params,
@@ -67,22 +67,26 @@ export async function manualCreatePurchaseOrder(params:{salesOrderGoodsSns:strin
 }
 
 export async function exportOrderList(params: IBaseFilterProps) {
-    return request.get(ApiPathEnum.ExportOrder, {
-        requestType: 'form',
-        params: params,
-        responseType:"blob",
-        parseResponse:false
-    }).then((response)=>{
-        const disposition = response.headers.get('content-disposition');
-        const fileName = decodeURI(disposition.substring(disposition.indexOf('filename=')+9, disposition.length));
-        response.blob().then((blob:Blob)=>{
-            const url = window.URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', fileName);
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
+    return request
+        .get(ApiPathEnum.ExportOrder, {
+            requestType: 'form',
+            params: params,
+            responseType: 'blob',
+            parseResponse: false,
         })
-    });
+        .then(response => {
+            const disposition = response.headers.get('content-disposition');
+            const fileName = decodeURI(
+                disposition.substring(disposition.indexOf('filename=') + 9, disposition.length),
+            );
+            response.blob().then((blob: Blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', fileName);
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+            });
+        });
 }
