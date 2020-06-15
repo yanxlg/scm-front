80541f981f0a34e80da6d2c8497cfe53b681c1bc
bbhuang <bbhuang@vova.com.hk>
2020-06-15 19:05:36 +0800

    'fix-treeselect'

--- src/components/MultipleSelect/MultipleSelect.tsx
+++ src/components/MultipleSelect/MultipleSelect.tsx
@@ -5,6 +5,8 @@ import { FormInstance } from 'antd/lib/form';
 import { FormItemLabelProps } from 'antd/es/form/FormItemLabel';
 import { TreeSelectProps } from 'antd/es/tree-select';
 
+import formStyles from 'react-components/es/JsonForm/_form.less';
+
 type IProps = {
     // label: string;
     name: string;
@@ -28,6 +30,7 @@ const MultipleSelect: React.FC<IProps> = ({
     maxTagCount = 6,
     treeNodeLabelProp = 'name',
     placeholder = '请选择',
+    dropdownClassName = formStyles.customTreeSelect,
     onChange,
     ...resetProps
 }) => {
@@ -111,6 +114,7 @@ const MultipleSelect: React.FC<IProps> = ({
                             maxTagCount={maxTagCount}
                             treeDefaultExpandAll={treeDefaultExpandAll}
                             placeholder={placeholder}
+                            dropdownClassName={dropdownClassName}
                             {...eventProps}
                             {...resetProps}
                         />
