d72dd5fe0ffe60afcc071b58dbd00ddbc6f58a9f
xlyan <xlyan@vova.com.hk>
2020-07-03 17:29:37 +0800

    source map

--- .umirc.ts
+++ .umirc.ts
@@ -15,7 +15,7 @@ const config = defineConfig({
         reportFiles: ['!src/.umi/!**', '!node_modules', 'src/!**!/!*.{ts,tsx}'],
     },*/
     hash: true,
-    devtool: dev ? 'source-map' : false,
+    devtool: dev ? 'source-map' : 'source-map',
     antd: {},
     cssModulesTypescriptLoader: {
         mode: 'emit',
