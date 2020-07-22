e84d30ca44450ffa21f7b91202a0337e0397c8ab
bbhuang <bbhuang@vova.com.hk>
2020-07-22 11:33:48 +0800

    'add上架店铺'

--- src/enums/LocalGoodsEnum.ts
+++ src/enums/LocalGoodsEnum.ts
@@ -34,15 +34,6 @@ export const versionStatusMap = {
 
 export const versionStatusList = transOptionList(versionStatusMap);
 
-// 上架渠道
-export const publishChannelStatusMap = {
-    1: 'VOVA',
-    2: 'FD',
-    3: 'AZ',
-};
-
-export const publishChannelStatusList = transOptionList(publishChannelStatusMap);
-
 // 上架状态
 export const publishStatusMap = {
     0: '上架中',
